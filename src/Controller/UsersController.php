00c276cb5d73985b7fdc3006b511a41b5b3f7ed6
takumi-no1s <hoya.takumi@no1s.biz>
2018-08-28 16:01:45 +0900

    modify usercontroller

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -26,7 +26,7 @@ use Cake\Log\Log;
  *
  * @link http://book.cakephp.org/3.0/en/controllers.html#the-app-controller
  */
-class UsersController extends Controller
+class UsersController extends AppController
 {
 
     /**
